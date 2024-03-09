dc34f124b33b95e8afd9fca7178807f3776cc00b
Mateus Fernandes <mateusowlfernandes@gmail.com>
2024-03-09 14:51:21 -0300

    Delete web-programming/catauto directory

--- web-programming/catauto/src/main/java/br/org/catolicasc/catauto/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.org.catolicasc.catauto.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.org.catolicasc.catauto.model.Pessoa;
-
-public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
-
-}
