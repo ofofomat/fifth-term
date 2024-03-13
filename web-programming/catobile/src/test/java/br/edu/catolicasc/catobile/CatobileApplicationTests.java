c2feb627101a4183c7555d40744b2385dd18c209
MATEUS FERNANDES <mateus.fernandes@catolicasc.edu.br>
2024-03-13 19:32:18 -0300

    Meu projeto

---
+++ web-programming/catobile/src/test/java/br/edu/catolicasc/catobile/CatobileApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.catolicasc.catobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatobileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
